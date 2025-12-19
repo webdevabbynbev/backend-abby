4f9943d6b411ffca152b6b6485876667bc156c79
iqbaliie <miqbalmahulana@gmail.com>
2025-12-19 18:38:12 +0700

    cloudinary

--- start/env.ts
+++ start/env.ts
@@ -28,7 +28,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
-
+  
   /*
   |----------------------------------------------------------
   | Variables for configuring the mail package
@@ -36,6 +36,8 @@ export default await Env.create(new URL('../', import.meta.url), {
   */
   SMTP_HOST: Env.schema.string(),
   SMTP_PORT: Env.schema.string(),
+
+  
 
   /*
   |----------------------------------------------------------
