84243b93f64e82785354cd78bf50a24a10699a68
presidentanjay <gentalav54@gmail.com>
2025-12-22 15:44:47 +0700

    update api

--- start/env.ts
+++ start/env.ts
@@ -1,14 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Environment variables service
-|--------------------------------------------------------------------------
-|
-| The `Env.create` method creates an instance of the Env service. The
-| service validates the environment variables and also cast values
-| to JavaScript data types.
-|
-*/
-
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
@@ -18,29 +7,27 @@ export default await Env.create(new URL('../', import.meta.url), {
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace', 'silent']),
 
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring database connection
-  |----------------------------------------------------------
-  */
   DB_HOST: Env.schema.string({ format: 'host' }),
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
 
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring the mail package
-  |----------------------------------------------------------
-  */
   SMTP_HOST: Env.schema.string(),
   SMTP_PORT: Env.schema.string(),
 
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring the drive package
-  |----------------------------------------------------------
-  */
   DRIVE_DISK: Env.schema.enum(['fs'] as const),
+
+  // =========================
+  // BITESHIP
+  // =========================
+  BITESHIP_BASE_URL: Env.schema.string(), // contoh: https://api.biteship.com
+  BITESHIP_API_KEY: Env.schema.string(),
+
+  // data toko (biar ongkir & order bisa dibuat)
+  COMPANY_NAME: Env.schema.string(),
+  COMPANY_EMAIL: Env.schema.string(),
+  COMPANY_PHONE: Env.schema.string(),
+  COMPANY_ADDRESS: Env.schema.string(),
+  COMPANY_POSTAL_CODE: Env.schema.string(),
 })
