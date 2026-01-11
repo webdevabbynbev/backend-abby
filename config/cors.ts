c280ebcb652cae2509c95d886d8aced857ea1f4c
iqbaliie <miqbalmahulana@gmail.com>
2026-01-11 14:12:45 +0700

    server allowing frontend local domain

--- config/cors.ts
+++ config/cors.ts
@@ -1,20 +1,16 @@
 import { defineConfig } from '@adonisjs/cors'
 
-/**
- * Configuration options to tweak the CORS policy. The following
- * options are documented on the official documentation website.
- *
- * https://docs.adonisjs.com/guides/security/cors
- */
-const corsConfig = defineConfig({
+export default defineConfig({
   enabled: true,
-  origin: ['http://localhost:5173',
-    'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+  ],
   credentials: true,
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   headers: true,
   exposeHeaders: ['Content-Type', 'Content-Length', 'X-Total-Count', 'Set-Cookie'],
   maxAge: 90,
 })
-
-export default corsConfig
