169048a3af9f84aa22befb4a1ae0774e8082f8be
presidentanjay <gentalav54@gmail.com>
2026-01-15 18:07:40 +0700

    Update 1768309185984_create_add_is_auto_to_discounts_table.ts

--- database/migrations/1768309185984_create_add_is_auto_to_discounts_table.ts
+++ database/migrations/1768309185984_create_add_is_auto_to_discounts_table.ts
@@ -1,18 +1,22 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
-export default class AddIsAutoToDiscounts extends BaseSchema {
+export default class CreateAddIsAutoToDiscountsTable extends BaseSchema {
   public async up() {
-    const has = await this.schema.hasColumn('discounts', 'is_auto')
+    const schema = (this.schema as any)
+    const has = await schema.hasColumn('discounts', 'is_auto')
+
     if (!has) {
       this.schema.alterTable('discounts', (table) => {
-        table.tinyint('is_auto').notNullable().defaultTo(0)
+        table.tinyint('is_auto').notNullable().defaultTo(1)
         table.index(['is_auto'])
       })
     }
   }
 
   public async down() {
-    const has = await this.schema.hasColumn('discounts', 'is_auto')
+    const schema = (this.schema as any)
+    const has = await schema.hasColumn('discounts', 'is_auto')
+
     if (has) {
       this.schema.alterTable('discounts', (table) => {
         table.dropIndex(['is_auto'])
